336024dd29cfc4e847c9d03d5b4259c92e34e735
gyro永不抽风 <1247006353@qq.com>
2022-03-19 22:14:19 +0800

    feat: add template

---
+++ project/app/src/main/java/top/gyrojeff/xposed/template/Entrance.java
@@ -0,0 +1,16 @@
+package top.gyrojeff.xposed.template;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class Entrance implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        XposedBridge.log("Loaded app: " + lpparam.packageName);
+    }
+}
